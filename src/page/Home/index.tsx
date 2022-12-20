9d38459cc06b4425ada40a77423ba676b21ad4a2
MatheusMoraes66 <matheus.csharp@hotmail.com>
2022-12-20 02:29:04 -0300

    Pesquisa usuario com enter

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import Button from '../../components/Button/Button';
 
 import style from '../../styles/Home.module.css'
 import { useUser } from '../../contexts/userContext';
@@ -9,6 +8,10 @@ const Home = () => {
     const { searchUser } = useUser();
     const [name, setName] = useState<string>("")
 
+    const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        searchUser(name)
+    }
     return (
         <div>
             <div className={style.container}>
@@ -16,10 +19,13 @@ const Home = () => {
                     <h1>Pomodore</h1>
                     <p>Digite o seu nome de usuario do GitHub, para poder acessar o aplicativo.</p>
                 </section>
-                <section className={style.search}>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                    <Button text='Buscar' action={() => searchUser(name)} />
-                </section>
+     
+                <form className={style.search} onSubmit={handlerSubmit }>
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                        <button className={style.btn} type="submit" hidden >Buscar</button>
+
+                    </form>
+            
             </div>
             <ErrorMessage message='ddd' isVisible={true} />
         </div>
